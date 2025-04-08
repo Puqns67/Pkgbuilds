12b2384e0783cf43b279f60627c9eaeaf0b8e9de
Puqns67 <me@puqns67.icu>
2025-04-08 19:35:52 +0800

    osu-bin: use sdl3 default again, remove .SRCINFO

--- packages/osu-bin/osu.bash
+++ packages/osu-bin/osu.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-#export OSU_SDL3=${OSU_SDL3:=true}
 export OSU_EXTERNAL_UPDATE_PROVIDER=true
+export OSU_SDL3=${OSU_SDL3:=true}
 
 if command -v gamemoderun &> /dev/null; then
     exec $(command -v gamemoderun) /usr/lib/osu/osu! "$@"
